9aec675c9dff5e662c876426184ae096953c89e8
AlexBelozertsev <kashim983@gmail.com>
2021-03-29 01:27:04 +0300

    actions step2 - done

--- src/redux/phonebook-actions.js
+++ src/redux/phonebook-actions.js
@@ -1,23 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
-import types from './phonebook-types';
+import { createAction } from '@reduxjs/toolkit';
 
-const addContact = ({ name, number }) => ({
-  type: types.ADD,
-  payload: {
-    id: uuidv4(),
-    name,
-    number,
-  },
-});
-
-const deleteContact = contactId => ({
-  type: types.DELETE,
-  payload: contactId,
-});
-
-const changeFilter = value => ({
-  type: types.CHANGE_FILTER,
-  payload: value,
-});
+const addContact = createAction('contact/add', ({ name, number }) => ({
+  payload: { id: uuidv4(), name, number },
+}));
+const deleteContact = createAction('contact/delete');
+const changeFilter = createAction('contact/changeFilter');
 
 export default { addContact, deleteContact, changeFilter };
